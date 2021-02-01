f396e010d6fcd095149ac230902385debe05407b
Prince Odia <princeodia@gmail.com>
2021-02-01 08:29:49 +0100

    Updated the onboarding screen

---
+++ app/src/main/java/com/khuosh/bella/OnboardingActivity.java
@@ -0,0 +1,21 @@
+package com.khuosh.bella;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Bella_Launcher_Onboarding);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(OnboardingActivity.this, LoginActivity));
+    }
+}
